02d7babf53649a9073279dd6458c32b86af927a6
Kyle Rioux <krioux5@uwo.ca>
2021-03-05 18:44:25 -0500

    Update data_explore_multi_radiologist.py
    prints as radiologist abnormalities are detected for unique scans and comments

--- Colab and Kaggle/data_explore_multi_radiologist.py
+++ Colab and Kaggle/data_explore_multi_radiologist.py
@@ -36,12 +36,17 @@ for entry in unique_25:
     unique_rads = [] # unique radiologists for this specific scan
     #iterate through entire csv
     for row in csv_file:
-        # entry matching current unique one looking for 
+        # check if this row matches entry considering currently 
         if row[0]==entry:
+            #check if radiologist is in list of radiologists 
             if row[3] not in unique_rads:
                 unique_rads.append(row[3])
+                print("first abnormality detected by radiologist: "+row[3])
+            else:
+                #this means the same radiologist is identifying multiple from the same scan
+                print("more than one abnormality detected from radiologist: "+row[3])
     print(unique_rads)
-    unique_rads = []
+    unique_rads = [] # resetting radiologists array 
 
 # ideas to show differences by radiologist:
     #-> number of things identified in scan by each radiologist (count how many entries from same R) 
