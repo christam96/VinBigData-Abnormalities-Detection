96fa2d14a680254d7e6e89dfa643211e68cad2ee
Kyle Rioux <krioux5@uwo.ca>
2021-03-09 12:07:41 -0500

    Update data_explore_multi_radiologist.py
    now runs on whole dataset- 15 times in chunks of 100- commented out  debug print statements

--- Exploratory Data Analysis/data_explore_multi_radiologist.py
+++ Exploratory Data Analysis/data_explore_multi_radiologist.py
@@ -24,71 +24,79 @@ for row in csv_file:
 print("number of unique entries: "+str(len(unique)))
 print("number of non-unique entries: "+str(len(non_unique)))
 
-# can loop through unique ones identifies
-unique_25 = unique[5:10] #first 25 unique entries 
-print(unique_25)
-
 disagreement_ctr = 0
 
-for entry in unique_25:
-    csv_file = csv.reader(open('train.csv', "r"), delimiter=",") # reset csv
-    header = next(csv_file) #skip first line
+i=0
 
-    print("currently on entry: "+ entry)
-    unique_rads = [] # unique radiologists for this specific scan
-    current_abnormalities = [] # tuple list storing abnormality and radiologist
-    #iterate through entire csv
-    for row in csv_file:
-        # check if this row matches entry considering currently 
-        if row[0]==entry:
-            current_abnormalities.append([row[3],int(row[2])])
-            #check if radiologist is in list of radiologists 
-            if row[3] not in unique_rads:
-                unique_rads.append(row[3])
-                print("first abnormality detected by radiologist: "+row[3])
-            else:
-                #this means the same radiologist is identifying multiple from the same scan
-                print("more than one abnormality detected from radiologist: "+row[3])
-    print("abnormalities found: "+str(current_abnormalities))
-    print("unique radiologists: "+str(unique_rads))
+while i<15: #0-15,000
+    unique_25 = unique[i*1000:(i+1)*1000]
 
-    findings = []
-    old_findings = []
-    bool_consensus = 1 
-    #comapre radiologist findings
-    for radiologist in unique_rads:
-        for each in current_abnormalities:
-            if each[0] == radiologist:
-                findings.append(each[1])
-        print("findings from radioloist: "+str(radiologist)+" is: "+str(findings))
+    # can loop through unique ones identifies
+    #unique_25 = unique[14000:15000] #first 25 unique entries 
+    #print(unique_25)
 
-        #check if findings is same as previous value
-        print(sorted(old_findings))
-        print(sorted(findings))
-        if len(old_findings) > 0: #cant compare first radiologist to no one before them!
-            if sorted(old_findings) == sorted(findings):
-                print("two radiologists agreed")
-            else:
-                print("two radiologists disagreed")
-                bool_consensus=0
-        else:
-            print("first radiologist abnormalities found")
+    for entry in unique_25:
+        csv_file = csv.reader(open('train.csv', "r"), delimiter=",") # reset csv
+        header = next(csv_file) #skip first line
 
-        
-        old_findings = findings #save old value
-        findings = [] #reset findings
+        #print("currently on entry: "+ entry)
+        unique_rads = [] # unique radiologists for this specific scan
+        current_abnormalities = [] # tuple list storing abnormality and radiologist
+        #iterate through entire csv
+        for row in csv_file:
+            # check if this row matches entry considering currently 
+            if row[0]==entry:
+                current_abnormalities.append([row[3],int(row[2])])
+                #check if radiologist is in list of radiologists 
+                if row[3] not in unique_rads:
+                    unique_rads.append(row[3])
+                    #print("first abnormality detected by radiologist: "+row[3])
+                #else:
+                    #this means the same radiologist is identifying multiple from the same scan
+                    #print("more than one abnormality detected from radiologist: "+row[3])
+        #print("abnormalities found: "+str(current_abnormalities))
+        #print("unique radiologists: "+str(unique_rads))
 
-        if bool_consensus == 0:
-            disagreement_ctr = disagreement_ctr+1
-            print("there was disagreement on this scan********************************************")
-            break # stop looking at radiologists for this scan
+        findings = []
+        old_findings = []
         bool_consensus = 1 
+        #comapre radiologist findings
+        for radiologist in unique_rads:
+            for each in current_abnormalities:
+                if each[0] == radiologist:
+                    findings.append(each[1])
+            #print("findings from radioloist: "+str(radiologist)+" is: "+str(findings))
 
-    unique_rads = [] # resetting radiologists array 
-print("out of " +str(len(unique_25))+ " samples, "+str(disagreement_ctr)+" had disagreement among radiologists")
+            #check if findings is same as previous value
+            #print(sorted(old_findings))
+            #print(sorted(findings))
+            if len(old_findings) > 0: #cant compare first radiologist to no one before them!
+                if sorted(old_findings) != sorted(findings):
+                    #print("two radiologists agreed")
+                    bool_consensus=0
+                #else:
+                    
+                    #print("two radiologists disagreed")
+                    #bool_consensus=0
+            #else:
+                #print("first radiologist abnormalities found")
 
-# ideas to show differences by radiologist:
-    #-> check how many samples have unanimous agreement between radiologists
-    #-> check difference in ROIs between rads
+            
+            old_findings = findings #save old value
+            findings = [] #reset findings
+
+            if bool_consensus == 0:
+                disagreement_ctr = disagreement_ctr+1
+                #print("there was disagreement on this scan********************************************")
+                break # stop looking at radiologists for this scan
+            bool_consensus = 1 
+
+        unique_rads = [] # resetting radiologists array 
+    print("in this batch of " +str(len(unique_25))+ " samples, "+str(disagreement_ctr)+" had disagreement among radiologists")
+    print("iteration: "+str(i))
+    i=i+1
+    # ideas to show differences by radiologist:
+        #-> check how many samples have unanimous agreement between radiologists
+        #-> check difference in ROIs between rads
 
 
